0e60d6ab1d21182aae7181fd0255893f927897d4
dimmWork <witty.sock3113@fastmail.com>
2022-08-23 18:23:49 +0200

    bad output formating

--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
-	fmt.Println(fizzBuzz(3))
+	fmt.Println(fizzBuzz(15))
 
 }
 
 func fizzBuzz(n int) []string {
+	var ans string
+	const (
+		f = "Fizz"
+		b = "Bizz"
+	)
 	res := make([]string, n, n)
 
-	for i := 0; i < n; i++ {
-		res[i] = "a"
+	for i := 1; i <= n; i++ {
+
+		if i%3 == 0 {
+			ans = f
+		}
+
+		if i%5 == 0 {
+			ans += b
+		}
+		if ans == "" {
+			res[i-1] = strconv.Itoa(i)
+		} else {
+			res[i-1] = ans
+		}
+
+		fmt.Println(i, ans)
+		ans = ""
+
 	}
 
 	return res
